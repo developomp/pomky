66b639d45a6b7a8d66d4ce2d5aa01b3571175385
developomp <developomp@gmail.com>
2021-12-25 15:27:25 +0900

    restructured memory data

--- src/memory.rs
+++ src/memory.rs
@@ -8,12 +8,18 @@ use sysinfo::{RefreshKind, System, SystemExt};
 use crate::{bar::draw_bar, could_not_get, util::kib_2_gb, MEMORY_UPDATE_INTERVAL};
 
 pub fn setup(builder: &Builder) {
-    let label_memory_usage: gtk::Label = builder
-        .object("label_memory_usage")
-        .expect(could_not_get!("label_memory_usage"));
+    let label_memory_used: gtk::Label = builder
+        .object("label_memory_used")
+        .expect(could_not_get!("label_memory_used"));
+    let label_memory_total: gtk::Label = builder
+        .object("label_memory_total")
+        .expect(could_not_get!("label_memory_total"));
     let label_memory_free: gtk::Label = builder
         .object("label_memory_free")
         .expect(could_not_get!("label_memory_free"));
+    let label_memory_percent: gtk::Label = builder
+        .object("label_memory_percent")
+        .expect(could_not_get!("label_memory_percent"));
     let memory_usage_bar: gtk::DrawingArea = builder
         .object("memory_usage_bar")
         .expect(could_not_get!("memory_usage_bar"));
@@ -22,8 +28,10 @@ pub fn setup(builder: &Builder) {
     sys.refresh_memory();
     update(
         &sys,
-        &label_memory_usage,
+        &label_memory_used,
+        &label_memory_total,
         &label_memory_free,
+        &label_memory_percent,
         &memory_usage_bar,
     );
 
@@ -31,8 +39,10 @@ pub fn setup(builder: &Builder) {
         sys.refresh_memory();
         update(
             &sys,
-            &label_memory_usage,
+            &label_memory_used,
+            &label_memory_total,
             &label_memory_free,
+            &label_memory_percent,
             &memory_usage_bar,
         );
 
@@ -42,25 +52,20 @@ pub fn setup(builder: &Builder) {
 
 fn update(
     sys: &System,
-    label_memory_usage: &gtk::Label,
+    label_memory_used: &gtk::Label,
+    label_memory_total: &gtk::Label,
     label_memory_free: &gtk::Label,
+    label_memory_percent: &gtk::Label,
     memory_usage_bar: &gtk::DrawingArea,
 ) {
     let mem_used = sys.used_memory();
     let mem_total = sys.total_memory();
     let ratio = mem_used as f64 / mem_total as f64;
 
-    label_memory_usage.set_text(
-        format!(
-            "{:.1} GiB / {:.1} GiB ({:.1}%)",
-            kib_2_gb(mem_used),
-            kib_2_gb(mem_total),
-            100.0 * ratio
-        )
-        .as_str(),
-    );
-
-    label_memory_free.set_text(format!("{:.1} GiB", kib_2_gb(sys.free_memory())).as_str());
+    label_memory_used.set_text(format!("{:.1} GB", kib_2_gb(mem_used)).as_str());
+    label_memory_total.set_text(format!("{:.1} GB", kib_2_gb(mem_total)).as_str());
+    label_memory_free.set_text(format!("{:.1} GB", kib_2_gb(sys.free_memory())).as_str());
+    label_memory_percent.set_text(format!("{:.1}%", 100.0 * ratio).as_str());
 
     draw_bar(&memory_usage_bar, 400, 6, ratio);
 }
