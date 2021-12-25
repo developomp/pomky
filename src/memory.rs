f8be1796c7114981e8a5aabacabd984233ee0d42
developomp <developomp@gmail.com>
2021-12-25 15:16:42 +0900

    added memory bar

--- src/memory.rs
+++ src/memory.rs
@@ -5,48 +5,62 @@ use gtk::{
 };
 use sysinfo::{RefreshKind, System, SystemExt};
 
-use crate::MEMORY_UPDATE_INTERVAL;
-use crate::{could_not_get, util::kib_2_gb};
+use crate::{bar::draw_bar, could_not_get, util::kib_2_gb, MEMORY_UPDATE_INTERVAL};
 
 pub fn setup(builder: &Builder) {
-    let mut sys = System::new_with_specifics(RefreshKind::new().with_memory());
-    sys.refresh_memory();
-
     let label_memory_usage: gtk::Label = builder
         .object("label_memory_usage")
         .expect(could_not_get!("label_memory_usage"));
     let label_memory_free: gtk::Label = builder
         .object("label_memory_free")
         .expect(could_not_get!("label_memory_free"));
+    let memory_usage_bar: gtk::DrawingArea = builder
+        .object("memory_usage_bar")
+        .expect(could_not_get!("memory_usage_bar"));
 
-    update_memory_usage(&label_memory_usage, &sys);
-    update_memory_free(&label_memory_free, &sys);
+    let mut sys = System::new_with_specifics(RefreshKind::new().with_memory());
+    sys.refresh_memory();
+    update(
+        &sys,
+        &label_memory_usage,
+        &label_memory_free,
+        &memory_usage_bar,
+    );
 
     glib::timeout_add_seconds_local(MEMORY_UPDATE_INTERVAL, move || {
         sys.refresh_memory();
-
-        update_memory_usage(&label_memory_usage, &sys);
-        update_memory_free(&label_memory_free, &sys);
+        update(
+            &sys,
+            &label_memory_usage,
+            &label_memory_free,
+            &memory_usage_bar,
+        );
 
         return glib::Continue(true);
     });
 }
 
-fn update_memory_usage(label: &gtk::Label, sys: &System) {
+fn update(
+    sys: &System,
+    label_memory_usage: &gtk::Label,
+    label_memory_free: &gtk::Label,
+    memory_usage_bar: &gtk::DrawingArea,
+) {
     let mem_used = sys.used_memory();
     let mem_total = sys.total_memory();
+    let ratio = mem_used as f64 / mem_total as f64;
 
-    label.set_text(
+    label_memory_usage.set_text(
         format!(
             "{:.1} GiB / {:.1} GiB ({:.1}%)",
             kib_2_gb(mem_used),
             kib_2_gb(mem_total),
-            100 * mem_used / mem_total
+            100.0 * ratio
         )
         .as_str(),
     );
-}
 
-fn update_memory_free(label: &gtk::Label, sys: &System) {
-    label.set_text(format!("{:.1} GiB", kib_2_gb(sys.free_memory())).as_str());
+    label_memory_free.set_text(format!("{:.1} GiB", kib_2_gb(sys.free_memory())).as_str());
+
+    draw_bar(&memory_usage_bar, 400, 6, ratio);
 }
