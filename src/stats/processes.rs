3881e8eddc95b40ff88177af819713bd01b19546
developomp <developomp@gmail.com>
2023-07-15 01:55:29 +0900

    refactor: replace indexed vars with arrays

--- src/stats/processes.rs
+++ src/stats/processes.rs
@@ -5,67 +5,41 @@ use sysinfo::{Pid, ProcessExt, ProcessRefreshKind, RefreshKind, System, SystemEx
 use crate::config;
 use crate::util::get_widget;
 
+const LABELS_N: usize = 3;
+
 pub fn setup(builder: &gtk::Builder) {
-    let process_percent1 = get_widget::<gtk::Label>("process_percent1", &builder);
-    let process_percent2 = get_widget::<gtk::Label>("process_percent2", &builder);
-    let process_percent3 = get_widget::<gtk::Label>("process_percent3", &builder);
-
-    let label_pid1 = get_widget::<gtk::Label>("pid1", &builder);
-    let label_pid2 = get_widget::<gtk::Label>("pid2", &builder);
-    let label_pid3 = get_widget::<gtk::Label>("pid3", &builder);
-
-    let process_name1 = get_widget::<gtk::Label>("process_name1", &builder);
-    let process_name2 = get_widget::<gtk::Label>("process_name2", &builder);
-    let process_name3 = get_widget::<gtk::Label>("process_name3", &builder);
-
     let mut sys = System::new_with_specifics(
         RefreshKind::new().with_processes(ProcessRefreshKind::new().with_cpu()),
     );
 
-    update(
-        &mut sys,
-        &process_percent1,
-        &process_percent2,
-        &process_percent3,
-        &label_pid1,
-        &label_pid2,
-        &label_pid3,
-        &process_name1,
-        &process_name2,
-        &process_name3,
-    );
-    glib::timeout_add_seconds_local(config::UPDATE_INTERVAL_PROCESS, move || {
-        update(
-            &mut sys,
-            &process_percent1,
-            &process_percent2,
-            &process_percent3,
-            &label_pid1,
-            &label_pid2,
-            &label_pid3,
-            &process_name1,
-            &process_name2,
-            &process_name3,
-        );
+    let process_percents: [gtk::Label; LABELS_N] = [
+        get_widget("process_percent1", &builder),
+        get_widget("process_percent2", &builder),
+        get_widget("process_percent3", &builder),
+    ];
+    let pids: [gtk::Label; LABELS_N] = [
+        get_widget("pid1", &builder),
+        get_widget("pid2", &builder),
+        get_widget("pid3", &builder),
+    ];
+    let process_names: [gtk::Label; LABELS_N] = [
+        get_widget("process_name1", &builder),
+        get_widget("process_name2", &builder),
+        get_widget("process_name3", &builder),
+    ];
 
+    update(&mut sys, &process_percents, &pids, &process_names);
+    glib::timeout_add_seconds_local(config::UPDATE_INTERVAL_PROCESS, move || {
+        update(&mut sys, &process_percents, &pids, &process_names);
         return glib::Continue(true);
     });
 }
 
 fn update(
     sys: &mut System,
-
-    process_percent1: &gtk::Label,
-    process_percent2: &gtk::Label,
-    process_percent3: &gtk::Label,
-
-    label_pid1: &gtk::Label,
-    label_pid2: &gtk::Label,
-    label_pid3: &gtk::Label,
-
-    process_name1: &gtk::Label,
-    process_name2: &gtk::Label,
-    process_name3: &gtk::Label,
+    process_percents: &[gtk::Label; 3],
+    pids: &[gtk::Label; 3],
+    process_names: &[gtk::Label; 3],
 ) {
     sys.refresh_processes_specifics(ProcessRefreshKind::new().with_cpu());
 
@@ -81,17 +55,11 @@ fn update(
 
     processes.sort_by(|a, b| b.cpu_usage.partial_cmp(&a.cpu_usage).unwrap());
 
-    process_percent1.set_text(format!("{:.1}%", processes[0].cpu_usage).as_str());
-    process_percent2.set_text(format!("{:.1}%", processes[1].cpu_usage).as_str());
-    process_percent3.set_text(format!("{:.1}%", processes[2].cpu_usage).as_str());
-
-    label_pid1.set_text(format!("{}", processes[0].pid).as_str());
-    label_pid2.set_text(format!("{}", processes[1].pid).as_str());
-    label_pid3.set_text(format!("{}", processes[3].pid).as_str());
-
-    process_name1.set_text(processes[0].name.as_str());
-    process_name2.set_text(processes[1].name.as_str());
-    process_name3.set_text(processes[2].name.as_str());
+    for i in 0..LABELS_N {
+        process_percents[i].set_text(format!("{:.1}%", processes[i].cpu_usage).as_str());
+        pids[i].set_text(format!("{}", processes[i].pid).as_str());
+        process_names[i].set_text(processes[i].name.as_str());
+    }
 }
 
 #[derive(Debug)]
