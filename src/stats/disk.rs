7ae94d4f956f87246b1385bc5ecceb90a00c67f4
developomp <developomp@gmail.com>
2023-07-15 12:28:52 +0900

    refactor: match disk stats with latest hardware

--- src/stats/disk.rs
+++ src/stats/disk.rs
@@ -1,6 +1,6 @@
 use gdk::glib;
 use gtk::prelude::*;
-use sysinfo::{Disk, DiskExt, RefreshKind, System, SystemExt};
+use sysinfo::{DiskExt, RefreshKind, System, SystemExt};
 
 use crate::custom_components::bar::build_bar;
 use crate::util::{b_2_gb, get_widget};
@@ -9,34 +9,25 @@ const UPDATE_INTERVAL_DISK: u32 = 10;
 
 pub fn setup(builder: &gtk::Builder) {
     // primary disk
-    let label_disk_root = get_widget("label_disk_root", &builder);
-    let label_disk_root_percent = get_widget("label_disk_root_percent", &builder);
-    let (disk_root_tx, disk_root_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
+    let label_disk_name = get_widget("label_disk_name", &builder);
+    let label_disk_usage = get_widget("label_disk_usage", &builder);
+    let label_disk_percent = get_widget("label_disk_percent", &builder);
+    let (disk_tx, disk_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
 
-    // secondary disk
-    let label_disk_data = get_widget("label_disk_data", &builder);
-    let label_disk_data_percent = get_widget("label_disk_data_percent", &builder);
-    let (disk_data_tx, disk_data_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-
-    build_bar(get_widget("disk_root_bar", &builder), 500, 6, disk_root_rx);
-    build_bar(get_widget("disk_data_bar", &builder), 500, 6, disk_data_rx);
+    build_bar(get_widget("disk_bar", &builder), 500, 6, disk_rx);
 
     update(
-        &label_disk_root,
-        &label_disk_root_percent,
-        &label_disk_data,
-        &label_disk_data_percent,
-        &disk_root_tx,
-        &disk_data_tx,
+        &label_disk_name,
+        &label_disk_usage,
+        &label_disk_percent,
+        &disk_tx,
     );
     glib::timeout_add_seconds_local(UPDATE_INTERVAL_DISK, move || {
         update(
-            &label_disk_root,
-            &label_disk_root_percent,
-            &label_disk_data,
-            &label_disk_data_percent,
-            &disk_root_tx,
-            &disk_data_tx,
+            &label_disk_name,
+            &label_disk_usage,
+            &label_disk_percent,
+            &disk_tx,
         );
 
         return glib::Continue(true);
@@ -44,12 +35,10 @@ pub fn setup(builder: &gtk::Builder) {
 }
 
 fn update(
-    label_disk_root: &gtk::Label,
-    label_disk_root_percent: &gtk::Label,
-    label_disk_data: &gtk::Label,
-    label_disk_data_percent: &gtk::Label,
-    disk_root_tx: &glib::Sender<f64>,
-    disk_data_tx: &glib::Sender<f64>,
+    label_disk_name: &gtk::Label,
+    label_disk_usage: &gtk::Label,
+    label_disk_percent: &gtk::Label,
+    disk_tx: &glib::Sender<f64>,
 ) {
     let mut sys = System::new_with_specifics(RefreshKind::new().with_disks().with_disks_list());
     sys.refresh_disks();
@@ -57,41 +46,22 @@ fn update(
     for disk in sys.disks() {
         match disk.mount_point().to_str() {
             Some("/") => {
-                update_disk(
-                    disk,
-                    &label_disk_root,
-                    &label_disk_root_percent,
-                    &disk_root_tx,
-                );
+                let total = disk.total_space();
+                let used = total - disk.available_space();
+                let ratio = used as f64 / total as f64;
+
+                label_disk_name
+                    .set_text(disk.name().to_str().expect("Failed to process disk name"));
+                label_disk_usage.set_text(&format!(
+                    "{:.1}  /  {:.1} GB",
+                    b_2_gb(used),
+                    b_2_gb(total)
+                ));
+                label_disk_percent.set_text(&format!("{:.1}%", 100.0 * ratio));
+                disk_tx.send(ratio).unwrap();
             }
 
-            Some("/media/pomp/data") => {
-                update_disk(
-                    disk,
-                    &label_disk_data,
-                    &label_disk_data_percent,
-                    &disk_data_tx,
-                );
-            }
-
-            Some(&_) => {}
-
-            None => {}
+            _ => {}
         }
     }
-}
-
-fn update_disk(
-    disk: &Disk,
-    label: &gtk::Label,
-    label_percent: &gtk::Label,
-    tx: &glib::Sender<f64>,
-) {
-    let total = disk.total_space();
-    let used = total - disk.available_space();
-    let ratio = used as f64 / total as f64;
-
-    label.set_text(&format!("{:.1}  /  {:.1} GB", b_2_gb(used), b_2_gb(total)));
-    label_percent.set_text(&format!("{:.1}%", 100.0 * ratio));
-    tx.send(ratio).unwrap();
 }
