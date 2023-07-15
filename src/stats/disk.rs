5e2e94c268ef792ca447aaedada9a57f635fd470
developomp <developomp@gmail.com>
2023-07-15 13:37:26 +0900

    refactor: update disk stats

--- src/stats/disk.rs
+++ src/stats/disk.rs
@@ -8,33 +8,21 @@ use crate::util::{b_2_gb, get_widget};
 const UPDATE_INTERVAL: u32 = 10;
 
 pub fn setup(builder: &gtk::Builder) {
-    let label_disk_name = get_widget("label_disk_name", &builder);
     let label_disk_usage = get_widget("label_disk_usage", &builder);
     let label_disk_percent = get_widget("label_disk_percent", &builder);
     let (disk_tx, disk_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
 
     build_bar(get_widget("disk_bar", &builder), 500, 6, disk_rx);
 
-    update(
-        &label_disk_name,
-        &label_disk_usage,
-        &label_disk_percent,
-        &disk_tx,
-    );
+    update(&label_disk_usage, &label_disk_percent, &disk_tx);
     glib::timeout_add_seconds_local(UPDATE_INTERVAL, move || {
-        update(
-            &label_disk_name,
-            &label_disk_usage,
-            &label_disk_percent,
-            &disk_tx,
-        );
+        update(&label_disk_usage, &label_disk_percent, &disk_tx);
 
         return glib::Continue(true);
     });
 }
 
 fn update(
-    label_disk_name: &gtk::Label,
     label_disk_usage: &gtk::Label,
     label_disk_percent: &gtk::Label,
     disk_tx: &glib::Sender<f64>,
@@ -42,25 +30,21 @@ fn update(
     let mut sys = System::new_with_specifics(RefreshKind::new().with_disks().with_disks_list());
     sys.refresh_disks();
 
-    for disk in sys.disks() {
-        match disk.mount_point().to_str() {
-            Some("/") => {
-                let total = disk.total_space();
-                let used = total - disk.available_space();
-                let ratio = used as f64 / total as f64;
-
-                label_disk_name
-                    .set_text(disk.name().to_str().expect("Failed to process disk name"));
-                label_disk_usage.set_text(&format!(
-                    "{:.1}  /  {:.1} GB",
-                    b_2_gb(used),
-                    b_2_gb(total)
-                ));
-                label_disk_percent.set_text(&format!("{:.1}%", 100.0 * ratio));
-                disk_tx.send(ratio).unwrap();
-            }
-
-            _ => {}
-        }
+    let mut total_bytes = 0;
+    let mut available_bytes = 0;
+    for partition in sys.disks() {
+        total_bytes += partition.total_space();
+        available_bytes += partition.available_space();
     }
+
+    let used_bytes = total_bytes - available_bytes;
+    let ratio = used_bytes as f64 / total_bytes as f64;
+
+    label_disk_usage.set_text(&format!(
+        "{:.1}  /  {:.1} GB",
+        b_2_gb(used_bytes),
+        b_2_gb(total_bytes)
+    ));
+    label_disk_percent.set_text(&format!("{:.1}%", 100.0 * ratio));
+    disk_tx.send(ratio).unwrap();
 }
