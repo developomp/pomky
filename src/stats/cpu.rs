acc0fe682fb8a863caee32edc25f6d7f86aca11b
developomp <developomp@gmail.com>
2023-07-15 13:02:50 +0900

    refactor: replace indexed vars with arrays

--- src/stats/cpu.rs
+++ src/stats/cpu.rs
@@ -6,106 +6,59 @@ use crate::custom_components::bar::build_bar;
 use crate::custom_components::graph::build_graph;
 use crate::util::get_widget;
 
+const NUM_CPU: usize = 8;
 const UPDATE_INTERVAL_CPU: u32 = 1;
+
+const BAR_WIDTH: i32 = 120;
+const BAR_HEIGHT: i32 = 6;
+
+const GRAPH_WIDTH: i32 = 120;
+const GRAPH_HEIGHT: i32 = 50;
 
 pub fn setup(builder: &gtk::Builder) {
     let mut sys = System::new_with_specifics(RefreshKind::new());
+
     let label_cpu_percent = get_widget("label_cpu_percent", &builder);
 
-    let (cpu_bar_tx, cpu_bar_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu0_bar_tx, cpu0_bar_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu1_bar_tx, cpu1_bar_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu2_bar_tx, cpu2_bar_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu3_bar_tx, cpu3_bar_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu4_bar_tx, cpu4_bar_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu5_bar_tx, cpu5_bar_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu6_bar_tx, cpu6_bar_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu7_bar_tx, cpu7_bar_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let cpu_percent_tx_channels: [Sender<f64>; 8] = [
-        cpu0_bar_tx,
-        cpu1_bar_tx,
-        cpu2_bar_tx,
-        cpu3_bar_tx,
-        cpu4_bar_tx,
-        cpu5_bar_tx,
-        cpu6_bar_tx,
-        cpu7_bar_tx,
-    ];
+    let (total_bar_tx, total_bar_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
+    let (bar_tx, bar_rx) = generate_cpu_core_channels();
 
-    let (cpu_graph_tx, cpu_graph_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu0_graph_tx, cpu0_graph_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu1_graph_tx, cpu1_graph_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu2_graph_tx, cpu2_graph_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu3_graph_tx, cpu3_graph_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu4_graph_tx, cpu4_graph_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu5_graph_tx, cpu5_graph_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu6_graph_tx, cpu6_graph_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    let (cpu7_graph_tx, cpu7_graph_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-
-    let cpu_graph_tx_channels: [Sender<u64>; 8] = [
-        cpu0_graph_tx,
-        cpu1_graph_tx,
-        cpu2_graph_tx,
-        cpu3_graph_tx,
-        cpu4_graph_tx,
-        cpu5_graph_tx,
-        cpu6_graph_tx,
-        cpu7_graph_tx,
-    ];
+    let (total_graph_tx, total_graph_rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
+    let (graph_tx, graph_rx) = generate_cpu_core_channels();
 
     build_bar(
         get_widget("drawing_area_cpu_percent_bar", &builder),
         500,
         6,
-        cpu_bar_rx,
+        total_bar_rx,
     );
     build_graph(
         get_widget("cpu_graph", &builder),
         500,
         100,
-        cpu_graph_rx,
+        total_graph_rx,
         Some(100),
     );
 
-    build_cpu_core_bars(
-        &builder,
-        cpu0_bar_rx,
-        cpu1_bar_rx,
-        cpu2_bar_rx,
-        cpu3_bar_rx,
-        cpu4_bar_rx,
-        cpu5_bar_rx,
-        cpu6_bar_rx,
-        cpu7_bar_rx,
-    );
-    build_cpu_core_graphs(
-        &builder,
-        cpu0_graph_rx,
-        cpu1_graph_rx,
-        cpu2_graph_rx,
-        cpu3_graph_rx,
-        cpu4_graph_rx,
-        cpu5_graph_rx,
-        cpu6_graph_rx,
-        cpu7_graph_rx,
-    );
+    build_cpu_core_bars(&builder, bar_rx);
+    build_cpu_core_graphs(&builder, graph_rx);
 
     update(
         &mut sys,
         &label_cpu_percent,
-        &cpu_bar_tx,
-        &cpu_graph_tx,
-        &cpu_percent_tx_channels,
-        &cpu_graph_tx_channels,
+        &total_bar_tx,
+        &total_graph_tx,
+        &bar_tx,
+        &graph_tx,
     );
     glib::timeout_add_seconds_local(UPDATE_INTERVAL_CPU, move || {
         update(
             &mut sys,
             &label_cpu_percent,
-            &cpu_bar_tx,
-            &cpu_graph_tx,
-            &cpu_percent_tx_channels,
-            &cpu_graph_tx_channels,
+            &total_bar_tx,
+            &total_graph_tx,
+            &bar_tx,
+            &graph_tx,
         );
 
         return glib::Continue(true);
@@ -138,152 +91,45 @@ fn update(
     label_cpu_percent.set_text(&format!("{:.1}%", total_percent / 8.0));
 }
 
-fn build_cpu_core_bars(
-    builder: &gtk::Builder,
-    cpu0_bar_rx: Receiver<f64>,
-    cpu1_bar_rx: Receiver<f64>,
-    cpu2_bar_rx: Receiver<f64>,
-    cpu3_bar_rx: Receiver<f64>,
-    cpu4_bar_rx: Receiver<f64>,
-    cpu5_bar_rx: Receiver<f64>,
-    cpu6_bar_rx: Receiver<f64>,
-    cpu7_bar_rx: Receiver<f64>,
-) {
-    const BAR_WIDTH: i32 = 120;
-    const BAR_HEIGHT: i32 = 6;
+fn generate_cpu_core_channels<T>() -> ([Sender<T>; NUM_CPU], [Receiver<T>; NUM_CPU]) {
+    let (tx0, rx0) = glib::MainContext::channel::<T>(glib::PRIORITY_DEFAULT);
+    let (tx1, rx1) = glib::MainContext::channel::<T>(glib::PRIORITY_DEFAULT);
+    let (tx2, rx2) = glib::MainContext::channel::<T>(glib::PRIORITY_DEFAULT);
+    let (tx3, rx3) = glib::MainContext::channel::<T>(glib::PRIORITY_DEFAULT);
+    let (tx4, rx4) = glib::MainContext::channel::<T>(glib::PRIORITY_DEFAULT);
+    let (tx5, rx5) = glib::MainContext::channel::<T>(glib::PRIORITY_DEFAULT);
+    let (tx6, rx6) = glib::MainContext::channel::<T>(glib::PRIORITY_DEFAULT);
+    let (tx7, rx7) = glib::MainContext::channel::<T>(glib::PRIORITY_DEFAULT);
 
-    build_bar(
-        get_widget("cpu0_bar", builder),
-        BAR_WIDTH,
-        BAR_HEIGHT,
-        cpu0_bar_rx,
-    );
-
-    build_bar(
-        get_widget("cpu1_bar", builder),
-        BAR_WIDTH,
-        BAR_HEIGHT,
-        cpu1_bar_rx,
-    );
-
-    build_bar(
-        get_widget("cpu2_bar", builder),
-        BAR_WIDTH,
-        BAR_HEIGHT,
-        cpu2_bar_rx,
-    );
-
-    build_bar(
-        get_widget("cpu3_bar", builder),
-        BAR_WIDTH,
-        BAR_HEIGHT,
-        cpu3_bar_rx,
-    );
-
-    build_bar(
-        get_widget("cpu4_bar", builder),
-        BAR_WIDTH,
-        BAR_HEIGHT,
-        cpu4_bar_rx,
-    );
-
-    build_bar(
-        get_widget("cpu5_bar", builder),
-        BAR_WIDTH,
-        BAR_HEIGHT,
-        cpu5_bar_rx,
-    );
-
-    build_bar(
-        get_widget("cpu6_bar", builder),
-        BAR_WIDTH,
-        BAR_HEIGHT,
-        cpu6_bar_rx,
-    );
-
-    build_bar(
-        get_widget("cpu7_bar", builder),
-        BAR_WIDTH,
-        BAR_HEIGHT,
-        cpu7_bar_rx,
+    return (
+        [tx0, tx1, tx2, tx3, tx4, tx5, tx6, tx7],
+        [rx0, rx1, rx2, rx3, rx4, rx5, rx6, rx7],
     );
 }
 
-fn build_cpu_core_graphs(
-    builder: &gtk::Builder,
-    cpu0_graph_rx: Receiver<u64>,
-    cpu1_graph_rx: Receiver<u64>,
-    cpu2_graph_rx: Receiver<u64>,
-    cpu3_graph_rx: Receiver<u64>,
-    cpu4_graph_rx: Receiver<u64>,
-    cpu5_graph_rx: Receiver<u64>,
-    cpu6_graph_rx: Receiver<u64>,
-    cpu7_graph_rx: Receiver<u64>,
-) {
-    const GRAPH_WIDTH: i32 = 120;
-    const GRAPH_HEIGHT: i32 = 50;
+fn build_cpu_core_bars(builder: &gtk::Builder, cpu_bar_rx: [Receiver<f64>; NUM_CPU]) {
+    let mut i = 0;
+    for rx in cpu_bar_rx {
+        build_bar(
+            get_widget(format!("cpu{}_bar", i).as_str(), builder),
+            BAR_WIDTH,
+            BAR_HEIGHT,
+            rx,
+        );
+        i += 1;
+    }
+}
 
-    build_graph(
-        get_widget("cpu0_graph", builder),
-        GRAPH_WIDTH,
-        GRAPH_HEIGHT,
-        cpu0_graph_rx,
-        Some(100),
-    );
-
-    build_graph(
-        get_widget("cpu1_graph", builder),
-        GRAPH_WIDTH,
-        GRAPH_HEIGHT,
-        cpu1_graph_rx,
-        Some(100),
-    );
-
-    build_graph(
-        get_widget("cpu2_graph", builder),
-        GRAPH_WIDTH,
-        GRAPH_HEIGHT,
-        cpu2_graph_rx,
-        Some(100),
-    );
-
-    build_graph(
-        get_widget("cpu3_graph", builder),
-        GRAPH_WIDTH,
-        GRAPH_HEIGHT,
-        cpu3_graph_rx,
-        Some(100),
-    );
-
-    build_graph(
-        get_widget("cpu4_graph", builder),
-        GRAPH_WIDTH,
-        GRAPH_HEIGHT,
-        cpu4_graph_rx,
-        Some(100),
-    );
-
-    build_graph(
-        get_widget("cpu5_graph", builder),
-        GRAPH_WIDTH,
-        GRAPH_HEIGHT,
-        cpu5_graph_rx,
-        Some(100),
-    );
-
-    build_graph(
-        get_widget("cpu6_graph", builder),
-        GRAPH_WIDTH,
-        GRAPH_HEIGHT,
-        cpu6_graph_rx,
-        Some(100),
-    );
-
-    build_graph(
-        get_widget("cpu7_graph", builder),
-        GRAPH_WIDTH,
-        GRAPH_HEIGHT,
-        cpu7_graph_rx,
-        Some(100),
-    );
+fn build_cpu_core_graphs(builder: &gtk::Builder, cpu_graph_rx: [Receiver<u64>; NUM_CPU]) {
+    let mut i = 0;
+    for rx in cpu_graph_rx {
+        build_graph(
+            get_widget(format!("cpu{}_graph", i).as_str(), builder),
+            GRAPH_WIDTH,
+            GRAPH_HEIGHT,
+            rx,
+            Some(100),
+        );
+        i += 1;
+    }
 }
